372cc1f5a4394b367c7d0c2073f81797267ce014
Junhyeok <elemapfelr@gmail.com>
2024-03-06 16:02:45 +0900

    메인 수정

--- src/lib/components/intro/Grid.svelte
+++ src/lib/components/intro/Grid.svelte
@@ -1,19 +1,21 @@
 <script>
 	import { goto } from '$app/navigation';
-	import { fly } from "svelte/transition";
+	import { fly, fade } from 'svelte/transition';
 
 	export let col;
 	export let row;
-	export let content;
+	export let title;
+	export let contents;
 	export let link;
+	export let titleImgSrc;
 
 	let clicked = false;
 
-	function gridClick(){
+	function gridClick() {
 		clicked = clicked ? false : true;
 	}
 
-	function goLink(){
+	function goLink() {
 		goto(link);
 	}
 </script>
@@ -23,14 +25,34 @@
 <!-- svelte-ignore a11y-mouse-events-have-key-events -->
 <div class="grid" style="grid-column: {col}; grid-row: {row}" on:click={gridClick}>
 	<div class="contents">
-		<div class="square"></div>
-		<h5>{content}</h5>
+		{#if clicked}
+			<div
+				class="details"
+				in:fly={{ x: 100, duration: 1000, delay: 300 }}
+				out:fly={{ x: 200, duration: 500 }}
+			>
+				<h5>{title}</h5>
+				<p>{contents}</p>
+				<button on:click={goLink}>Go!</button>
+			</div>
+		{:else}
+			<div
+				class="title"
+				in:fade={{ duration: 200, delay: 500 }}
+				out:fly={{ y: -100, duration: 300 }}
+			>
+				<div class="square">
+					<img src={titleImgSrc} alt="title logo" />
+				</div>
+				<h4>{title}</h4>
+			</div>
+		{/if}
 	</div>
 </div>
 
 <style lang="scss">
-	@mixin keyframes($name){
-		@keyframes #{$name}{
+	@mixin keyframes($name) {
+		@keyframes #{$name} {
 			@content;
 		}
 	}
@@ -41,49 +63,99 @@
 		padding: 30px;
 		box-sizing: border-box;
 		background-color: transparentize(#fff, 0.3);
-		position: relative;
-		cursor: pointer;
-		
-		.contents{
-			transition: all 1s cubic-bezier(0.075, 0.82, 0.165, 1);
-			position: absolute;
-			top: 50%;
-			left: 50%;
-			transform: translate(-50%, -50%);
-			.square{
-				width: 100px;
-				height: 100px;
-				background-color: #fff;
-				border-radius: 15px;
-				transition: all 1s cubic-bezier(0.075, 0.82, 0.165, 1);
-			}
+		cursor: default;
+		overflow: hidden;
 
-			h5{
-				margin-top: 5px;
-				font-size: 1rem;
-				text-align: center;
+		.contents {
+			width: 100%;
+			height: 100%;
+			position: relative;
+			.title {
+				position: absolute;
+				top: 50%;
+				left: 50%;
+				transform: translate(-50%, -50%);
 				transition: all 1s cubic-bezier(0.075, 0.82, 0.165, 1);
+				.square {
+					width: 90px;
+					height: 90px;
+					// padding: 10px;
+					border-radius: 30px;
+					box-sizing: border-box;
+					overflow: hidden;
+					margin: 0 auto;
+					background-color: #fff;
+					transition: all 1s cubic-bezier(0.075, 0.82, 0.165, 1);
+
+					img {
+						width: 100%;
+						height: auto;
+						margin: 0 auto;
+					}
+				}
+
+				h4 {
+					margin-top: 5px;
+					font-size: 1rem;
+					text-align: center;
+					transition: all 1s cubic-bezier(0.075, 0.82, 0.165, 1);
+				}
+			}
+			.details {
+				width: 100%;
+				max-height: 100%;
+				position: absolute;
+				top: 50%;
+				left: 50%;
+				transform: translate(-50%, -50%);
+				overflow: hidden;
+				h5 {
+					text-align: left;
+					font-size: 1.5rem;
+					margin-bottom: 10px;
+				}
+				p {
+					text-align: left;
+				}
+				button {
+					float: right;
+					margin-top: 10px;
+					padding: 5px 20px 4px;
+					border: none;
+					outline: none;
+					background: linear-gradient(135deg, #81ffef 10%, #f067b4 100%);
+					background-size: 200% 200%;
+					color: #202020;
+					background-position: 0% 50%;
+					cursor: pointer;
+
+					&:hover {
+						animation: gradientMove 15s linear infinite;
+					}
+
+					@include keyframes(gradientMove) {
+						0% {
+							background-position: 0% 50%;
+						}
+						50% {
+							background-position: 100% 50%;
+						}
+						100% {
+							background-position: 0% 50%;
+						}
+					}
+				}
 			}
 		}
 
-		@include keyframes(shaking){
-			0% { transform: translate(-50%, -50%) rotate(0deg); }
-			5% { transform: translate(-50%, -50%) rotate(5deg); }
-			7.5% { transform: translate(-50%, -50%) rotate(0eg); }
-			10% { transform: translate(-50%, -50%) rotate(-5deg); }
-			12.5% { transform: translate(-50%, -50%) rotate(0deg); }
-			15% { transform: translate(-50%, -50%) rotate(5deg); }
-			17.5% { transform: translate(-50%, -50%) rotate(0eg); }
-			20% { transform: translate(-50%, -50%) rotate(-5deg); }
-			22.5% { transform: translate(-50%, -50%) rotate(0deg); }
-			25% { transform: translate(-50%, -50%) rotate(5deg); }
-			30% { transform: translate(-50%, -50%) rotate(0deg); }
-			100% { transform: translate(-50%, -50%) rotate(0deg); }
-		}
-
-		&:hover{
-			.contents{
-				animation: shaking 1s linear infinite;
+		&:hover {
+			.contents {
+				.title {
+					transform: translate(-50%, -50%) scale(1.05);
+					.square {
+						box-shadow: 0 0 20px #7775;
+					}
+				}
 			}
 		}
 	}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,53 +1,71 @@
 <script>
 	import Grid from '$lib/components/intro/Grid.svelte';
+	import { fade } from 'svelte/transition';
+	import gomoku from '$lib/img/gomoku.svg';
 
 	let grids = [
 		{
 			col: '1/3',
 			row: '1/3',
-			content: 'grid cell',
+			title: 'grid cell',
+			contents:
+				'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam labore fugiat odit, consequuntur obcaecati sunt harum suscipit nemo minima hic? Facere tempora eaque voluptas adipisci at omnis labore repellat. Culpa?',
 			link: ''
 		},
 		{
 			col: '3/5',
 			row: '1/2',
-			content: 'grid cell',
+			title: 'grid cell',
+			contents:
+				'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam labore fugiat odit, consequuntur obcaecati sunt harum suscipit nemo minima hic? Facere tempora eaque voluptas adipisci at omnis labore repellat. Culpa?',
 			link: ''
 		},
 		{
 			col: '3/4',
 			row: '2/3',
-			content: 'Omock',
-			link: '/omock'
+			title: 'Omock',
+			contents: 'This is a simple gomoku that can be played online. Do you want to play?',
+			link: '/omock',
+			titleImgSrc: gomoku
 		},
 		{
 			col: '4/5',
 			row: '2/3',
-			content: 'grid cell',
+			title: 'grid cell',
+			contents:
+				'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam labore fugiat odit, consequuntur obcaecati sunt harum suscipit nemo minima hic? Facere tempora eaque voluptas adipisci at omnis labore repellat. Culpa?',
 			link: ''
 		},
 		{
 			col: '1/2',
 			row: '3/4',
-			content: 'grid cell',
+			title: 'grid cell',
+			contents:
+				'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam labore fugiat odit, consequuntur obcaecati sunt harum suscipit nemo minima hic? Facere tempora eaque voluptas adipisci at omnis labore repellat. Culpa?',
 			link: ''
 		},
 		{
 			col: '1/2',
 			row: '4/5',
-			content: 'grid cell',
+			title: 'grid cell',
+			contents:
+				'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam labore fugiat odit, consequuntur obcaecati sunt harum suscipit nemo minima hic? Facere tempora eaque voluptas adipisci at omnis labore repellat. Culpa?',
 			link: ''
 		},
 		{
 			col: '2/3',
 			row: '3/5',
-			content: 'grid cell',
+			title: 'grid cell',
+			contents:
+				'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam labore fugiat odit, consequuntur obcaecati sunt harum suscipit nemo minima hic? Facere tempora eaque voluptas adipisci at omnis labore repellat. Culpa?',
 			link: ''
 		},
 		{
 			col: '3/5',
 			row: '3/5',
-			content: 'grid cell',
+			title: 'grid cell',
+			contents:
+				'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam labore fugiat odit, consequuntur obcaecati sunt harum suscipit nemo minima hic? Facere tempora eaque voluptas adipisci at omnis labore repellat. Culpa?',
 			link: ''
 		}
 	];
@@ -55,7 +73,14 @@
 
 <div class="gridArea">
 	{#each grids as item}
-		<Grid col={item.col} row={item.row} content={item.content} link={item.link}></Grid>
+		<Grid
+			col={item.col}
+			row={item.row}
+			title={item.title}
+			contents={item.contents}
+			link={item.link}
+			titleImgSrc={item.titleImgSrc}
+		></Grid>
 	{/each}
 </div>
 
