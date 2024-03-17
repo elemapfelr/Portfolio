c3a8c9dfb3389697cb270088c51c30955b23aee3
JunHyeok <elemapfelr@gmail.com>
2024-03-17 16:47:00 +0900

    고의적인 지연 로딩

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,43 +1,62 @@
 <script>
-	import { fly } from "svelte/transition";
-	import { cubicIn, cubicOut } from "svelte/easing";
-	import "$lib/scss/style.scss";
-	import Nav from "$lib/components/common/Nav.svelte";
-	import Loading from "$lib/components/common/Loading.svelte";
-	import { onMount } from "svelte";
-	export let data;
+  import { fly } from "svelte/transition";
+  import { cubicIn, cubicOut } from "svelte/easing";
+  import "$lib/scss/style.scss";
+  import Nav from "$lib/components/common/Nav.svelte";
+  import Loading from "$lib/components/common/Loading.svelte";
+  import { onMount } from "svelte";
+  export let data;
 
-	let loading = true;
-	onMount(() => {
-		loading = false;
-	});
+  let s_time = new Date();
+  let loading = true;
+  onMount(() => {
+    if (!sessionStorage.getItem("loaded")) {
+      let e_time = new Date();
+      // 일부러 로딩 보여주기
+      if (e_time - s_time < 2000) {
+        setTimeout(
+          () => {
+            loading = false;
+          },
+          2000 - (e_time - s_time)
+        );
+      } else {
+        loading = false;
+      }
+      sessionStorage.setItem("loaded", 1);
+    } else {
+      setTimeout(() => {
+        loading = false;
+      }, 300);
+    }
+  });
 </script>
 
 <main>
-	{#if loading}
-		<Loading />
-	{/if}
-	<Nav {loading} />
-	{#key data.pathname}
-		<div
-			class="slotArea"
-			in:fly={{ easing: cubicOut, y: 10, duration: 300, delay: 400 }}
-			out:fly={{ easing: cubicIn, y: -10, duration: 300 }}
-		>
-			<slot />
-		</div>
-	{/key}
+  {#if loading}
+    <Loading />
+  {/if}
+  <Nav {loading} />
+  {#key data.pathname}
+    <div
+      class="slotArea"
+      in:fly={{ easing: cubicOut, y: 10, duration: 300, delay: 400 }}
+      out:fly={{ easing: cubicIn, y: -10, duration: 300 }}
+    >
+      <slot />
+    </div>
+  {/key}
 </main>
 
 <style lang="scss">
-	.slotArea {
-		height: 100dvh;
-		overflow: hidden;
-		transition: all 0.3s;
-	}
-	@media screen and (max-width: 720px) {
-		.slotArea {
-			height: calc(100dvh - 50px);
-		}
-	}
+  .slotArea {
+    height: 100dvh;
+    overflow: hidden;
+    transition: all 0.3s;
+  }
+  @media screen and (max-width: 720px) {
+    .slotArea {
+      height: calc(100dvh - 50px);
+    }
+  }
 </style>
