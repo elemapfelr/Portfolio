b8b2533f3649f986e681bbd9ec7ebb26986cb390
Junhyeok <elemapfelr@gmail.com>
2024-04-02 17:44:18 +0900

    데모 추가

--- server/app.js
+++ server/app.js
@@ -8,7 +8,13 @@ setupWebSocket(3001);
 // Svelte 정적 파일 서빙
 app.use(express.static('public')); // 'public'은 빌드된 파일이 있는 디렉토리
 
-// 모든 요청에 대해 index.html을 반환
+app.get('/demo/:dir/:page', (req, res)=> {
+	const dir = req.params.dir;
+	const page = req.params.page;
+	res.sendFile(path.resolve(__dirname, 'public', dir, `${page}.html`))
+})
+
+// 그 외 모든 요청에 대해 index.html을 반환
 app.get('*', (req, res) => {
 	res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
