ccd831f37d756301c576eb5b89ab1168efb7ec33
Junhyeok <elemapfelr@gmail.com>
2024-04-17 17:20:02 +0900

    app.js에 openai 추가

--- server/app.js
+++ server/app.js
@@ -1,16 +1,36 @@
-const express = require('express');
+const OpenAI = require("openai");
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+const express = require("express");
 const app = express();
-const path = require('path');
-const { setupWebSocket } = require('./controller/webSocketController');
+const path = require("path");
+const { setupWebSocket } = require("./controller/webSocketController");
+
+async function main() {
+	const completion = await openai.chat.completions.create({
+		messages: [{ role: "system", content: "You are a helpful assistant." }],
+		model: "gpt-3.5-turbo",
+	});
+
+	console.log(completion.choices[0]);
+}
+
+// main();
 
 setupWebSocket(3001);
 
 // Svelte 정적 파일 서빙
-app.use(express.static('public/build')); // 'public'은 빌드된 파일이 있는 디렉토리
+app.use(express.static("public/build")); // 'public'은 빌드된 파일이 있는 디렉토리
 
-// 모든 요청에 대해 index.html을 반환
-app.get('*', (req, res) => {
-	res.sendFile(path.resolve(__dirname, 'public', 'build', 'index.html'));
+app.get("/demo/:dir/:page", (req, res) => {
+	const dir = req.params.dir;
+	const page = req.params.page;
+	res.sendFile(path.resolve(__dirname, "public", "demo", dir, `${page}.html`));
+});
+
+// 그 외 모든 요청에 대해 index.html을 반환
+app.get("*", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "public", "build", "index.html"));
+	console.log("Connected");
 });
 
 const PORT = process.env.PORT || 3000;
