629696db6e37e7f0f9a85b9c7a8aadbd8f83fc3b
JunHyeok <elemapfelr@gmail.com>
2024-04-19 21:51:27 +0900

    winston 제거, pm2로 로그 관리

--- server/app.js
+++ server/app.js
@@ -2,32 +2,38 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const { setupWebSocket } = require("./controller/webSocketController");
-const logger = require("./middleware/logger");
 const useragent = require("useragent");
 
 setupWebSocket(3001);
 app.use(express.json({ limit: "100mb" }));
 app.use("/api/gpt", require("./routes/openaiGPT"));
 
+app.get("/demo/:dir/:page", (req, res) => {
+  const dir = req.params.dir;
+  const page = req.params.page;
+  res.sendFile(path.resolve(__dirname, "public", "demo", dir, `${page}.html`));
+});
+
+// 접속자 로그 기록
+app.use((req, res, next) => {
+  if (req.path === "/") {
+    const agent = useragent.parse(req.headers["user-agent"]);
+    let identityLog = `Accessor identity \n - IP: ${
+      req.headers["x-forwarded-for"] || req.ip
+    } \n - Device: ${agent.device.toString()} \n - OS: ${agent.os.toString()} \n - Browser: ${agent.toAgent()}`;
+    console.log("Connected");
+    console.log(identityLog);
+  }
+  next();
+});
+
 // Svelte 정적 파일 서빙
 app.use(express.static("public/build")); // 'public'은 빌드된 파일이 있는 디렉토리
 
-app.get("/demo/:dir/:page", (req, res) => {
-	const dir = req.params.dir;
-	const page = req.params.page;
-	res.sendFile(path.resolve(__dirname, "public", "demo", dir, `${page}.html`));
-});
-
-// 그 외 모든 요청에 대해 index.html을 반환
+// 그 외 모든 요청에 대해 index.html을 반환 (혹시 몰라)
 app.get("*", (req, res) => {
-	const agent = useragent.parse(req.headers["user-agent"]);
-	logger.info(
-		`Accessor identity \n - IP: ${
-			req.headers["x-forwarded-for"] || req.ip
-		} \n - Device: ${agent.device.toString()} \n - OS: ${agent.os.toString()} \n - Browser: ${agent.toAgent()}`
-	);
-	res.sendFile(path.resolve(__dirname, "public", "build", "index.html"));
-	console.log("Connected");
+  console.log("Access *");
+  res.sendFile(path.resolve(__dirname, "public", "build", "index.html"));
 });
 
 const PORT = process.env.PORT || 3000;
